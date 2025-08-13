203a79f2401aedac014b290b9a1022e72393a815
Renato Moraees <renatomoraesdossantos@gmail.com>
2025-08-13 09:27:06 -0300

    Adicionando comentários

--- app/config.py
+++ app/config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    """
+        Classe responsável por definir as constantes através do arquivo de propriedades.
+    """
     DB_LINK = os.getenv("DB_LINK")
     OPENAI_MODEL = os.getenv("OPENAI_MODEL")
     DEBUG = os.getenv("DEBUG", "True").lower() == "true"
